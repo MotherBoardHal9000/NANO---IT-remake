2ad827789cf165445a4772bbb0894cdf1291706f
Lee jung woon <149851043+MotherBoardHal9000@users.noreply.github.com>
2026-01-05 15:07:45 +0900

    Refactor Canvas and GlowParticle classes

--- js/mainGradient.js
+++ js/mainGradient.js
@@ -136,15 +136,18 @@ class GlowParticle {
 }
 
 
-if (matchMedia("only screen and (max-width: 767px)").matches){
+if (matchMedia("only screen and (max-width: 768px)").matches){
   class Canvas {
   constructor() {
     this.canvas = document.createElement("canvas"); //this는 새로 생성되는 object를 뜻함 즉 this.canvas는 뭐냐 새로생성되는 object의 canvas라는 속성에는 여기적힌 canvas란 값을 대입해 주세요
     //  //this 는 새로 생성되는 object를 뜻한다. this.age = 15 이렇게 새로 생성되는 object에 값 부여 가능
-    this.canvas.style.display = none;
-
+    this.canvas.style.width = 90 +"vw";
+    this.canvas.style.height = 60+"vh";
+    this.canvas.style.margin = "none";
+    this.canvas.style.position = 'absolute';
     this.ctx = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
+
 
     this.pixelRatio = document.body.PixelRatio > 1 ? 2 : 1;
     this.totalParticles = 10;
